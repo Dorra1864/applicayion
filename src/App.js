e5ed204e99985bfc0cff13bcabe86045e7cb3f03
Dorra1864 <bentilidorra5@gmail.com>
2023-11-30 16:13:45 +0100

    fghj

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Vente from './page/Vente';
+function App() {
+  return (
+    <div className="App">
+  <Vente/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/page/Vente.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.css';
+import './vente.css'
+export default function Vente() {
+  return (
+    <div>
+      <h1> bonjour </h1>
+      <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  </table>
+
+<nav class="navbar bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+  </div>
+</nav>
+
+<nav class="navbar bg-body-tertiary">
+  <div class="container-fluid">
+    <span class="navbar-brand mb-0 h1">Navbar</span>
+  </div>
+</nav>
+    </div>
+  )
+}
